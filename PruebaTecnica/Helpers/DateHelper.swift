e40c2b70e19e97321c919aa8fcbde297dd6fd794
Yoel Jimenez del Valle <yoel07@gmail.com>
2021-07-14 23:14:13 -0400

    Date helper with dateFomatter

--- PruebaTecnica/Helpers/DateHelper.swift
+++ PruebaTecnica/Helpers/DateHelper.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+class DateHelper: NSObject {
+    static func getDate(date: String) -> String {
+        let formater = DateFormatter()
+        formater.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let dateO = formater.date(from: date) ?? Date()
+        formater.dateStyle = .medium
+        formater.doesRelativeDateFormatting = true
+        return formater.string(from: dateO)
+    }
+}
